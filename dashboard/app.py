70d808b892ee98bd539bd3f54a8175a0f2468b36
aubreygodjedo <aubreygodjedo1994@gmail.com>
2026-04-27 12:08:05 +0100

    Initial project structure

---
+++ dashboard/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Bénin Insights Challenge",
+    page_icon="🇧🇯",
+    layout="wide"
+)
+
+st.title("🇧🇯 Bénin Insights Challenge 2026")
+st.write("Dashboard interactif basé sur les données GDELT relatives au Bénin.")
